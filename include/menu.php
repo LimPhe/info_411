ecc3264857a9d4bc3a92961a2858492cea899896
LimPhe <favreauphelim@gmail.com>
2025-03-12 10:11:55 +0100

    test

--- include/menu.php
+++ include/menu.php
@@ -1,8 +1,5 @@
 <nav>
     <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="catalogue.php">Catalogue</a></li>
-        <li><a href="contact.php">Contact</a></li>
-        <li><a href="mentions-legales.php">Mentions légales</a></li>
     </ul>
 </nav>
